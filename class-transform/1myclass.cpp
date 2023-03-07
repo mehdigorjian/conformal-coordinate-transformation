b6d78eef04c1a5ecaddd2c07b4d7bfdb721d92bd
mehdigorjian <mehdi.gorjian@gmail.com>
2023-03-07 14:26:16 -0600

    class transform updated

---
+++ class-transform/1myclass.cpp
@@ -0,0 +1,6 @@
+#include "Transformation.h"
+myclass::myclass() {
+}
+void myclass::dummy(std::string s) {
+    std::cout << s << std::endl;
+}
--- class-transform/main.cpp
+++ class-transform/main.cpp
@@ -3,110 +3,7 @@
 #include <iostream>
 #include <vector>
 
-void calculateA(Eigen::MatrixXd& A, std::vector<Eigen::Vector3d>& ptsA) {
-    for (int i = 0; i < ptsA.size(); i++) {
-        A(0, 0) += ptsA[i][0] * ptsA[i][0];
-        A(0, 1) += ptsA[i][0] * ptsA[i][1];
-        A(0, 2) += ptsA[i][0] * ptsA[i][2];
-        A(0, 3) += ptsA[i][0];
-        A(1, 0) += ptsA[i][0] * ptsA[i][1];
-        A(1, 1) += ptsA[i][1] * ptsA[i][1];
-        A(1, 2) += ptsA[i][1] * ptsA[i][2];
-        A(1, 3) += ptsA[i][1];
-        A(2, 0) += ptsA[i][0] * ptsA[i][2];
-        A(2, 1) += ptsA[i][1] * ptsA[i][2];
-        A(2, 2) += ptsA[i][2] * ptsA[i][2];
-        A(2, 3) += ptsA[i][2];
-        A(3, 0) += ptsA[i][0];
-        A(3, 1) += ptsA[i][1];
-        A(3, 2) += ptsA[i][2];
-    }
-    A(3, 3) = (double)ptsA.size();
-}
-
-void calculateC1(Eigen::MatrixXd& C1, std::vector<Eigen::Vector3d>& ptsA, std::vector<Eigen::Vector3d>& ptsB) {
-    for (int i = 0; i < ptsA.size(); i++) {
-        C1(0, 0) += ptsB[i][0] * ptsA[i][0];
-        C1(1, 0) += ptsB[i][0] * ptsA[i][1];
-        C1(2, 0) += ptsB[i][0] * ptsA[i][2];
-        C1(3, 0) += ptsB[i][0];
-    }
-}
-
-void calculateC2(Eigen::MatrixXd& C2, std::vector<Eigen::Vector3d>& ptsA, std::vector<Eigen::Vector3d>& ptsB) {
-    for (int i = 0; i < ptsA.size(); i++) {
-        C2(0, 0) += ptsB[i][1] * ptsA[i][0];
-        C2(1, 0) += ptsB[i][1] * ptsA[i][1];
-        C2(2, 0) += ptsB[i][1] * ptsA[i][2];
-        C2(3, 0) += ptsB[i][1];
-    }
-}
-
-void calculateC3(Eigen::MatrixXd& C3, std::vector<Eigen::Vector3d>& ptsA, std::vector<Eigen::Vector3d>& ptsB) {
-    for (int i = 0; i < ptsA.size(); i++) {
-        C3(0, 0) += ptsB[i][2] * ptsA[i][0];
-        C3(1, 0) += ptsB[i][2] * ptsA[i][1];
-        C3(2, 0) += ptsB[i][2] * ptsA[i][2];
-        C3(3, 0) += ptsB[i][2];
-    }
-}
-
-void calculateTransformMatrix(Eigen::MatrixXd& transformMat, double& phi, double& psi, double& theta, Eigen::MatrixXd& A, Eigen::MatrixXd& C1, Eigen::MatrixXd& C2, Eigen::MatrixXd& C3, std::vector<Eigen::Vector3d>& ptsA, std::vector<Eigen::Vector3d>& ptsB) {
-    calculateA(A, ptsA);
-    calculateC1(C1, ptsA, ptsB);
-    calculateC2(C2, ptsA, ptsB);
-    calculateC3(C3, ptsA, ptsB);
-
-    // transformation matrix
-    Eigen::MatrixXd row1(4, 1), row2(4, 1), row3(4, 1), temp(4, 1);
-    row1 = A.inverse() * C1;
-    row2 = A.inverse() * C2;
-    row3 = A.inverse() * C3;
-
-    transformMat(3, 0) = 0.0;
-    transformMat(3, 1) = 0.0;
-    transformMat(3, 2) = 0.0;
-    transformMat(3, 3) = 1.0;
-
-    for (int i = 0; i < 3; i++) {
-        if (i == 0) temp = row1;
-        if (i == 1) temp = row2;
-        if (i == 2) temp = row3;
-        for (int j = 0; j < 4; j++) {
-            transformMat(i, j) = temp(j, 0);
-        }
-    }
-    phi = std::atan2(transformMat(1, 2), transformMat(2, 2));
-    psi = std::atan2(transformMat(0, 1), transformMat(0, 0));
-    theta = std::atan2(-transformMat(0, 2), transformMat(0, 1) / std::sin(psi));
-}
-
-void transformPointFromSysAtoSysB(Eigen::MatrixXd& transformMat, Eigen::Vector3d& pt, Eigen::Vector3d& transformedPt) {
-    Eigen::MatrixXd tempPt(4, 1);
-    tempPt << pt[0], pt[1], pt[2], 1.0;
-    tempPt = transformMat * tempPt;
-    transformedPt[0] = tempPt(0, 0);
-    transformedPt[1] = tempPt(1, 0);
-    transformedPt[2] = tempPt(2, 0);
-}
-
-void generatedPointsEquivaletPtsB(Eigen::MatrixXd& transformMat, std::vector<Eigen::Vector3d>& ptsA, std::vector<Eigen::Vector3d>& ptsB, std::vector<Eigen::Vector3d>& ptsBcalculated) {
-    for (int i = 0; i < ptsA.size(); i++) {
-        Eigen::Vector3d calcPt;
-        transformPointFromSysAtoSysB(transformMat, ptsA[i], calcPt);
-        ptsBcalculated.push_back(calcPt);
-    }
-}
-
-double errorCalculation(std::vector<Eigen::Vector3d>& ptsB, std::vector<Eigen::Vector3d>& ptsBcalculated) {
-    double sum = 0.0;
-    for (int i = 0; i < ptsB.size(); i++) {
-        Eigen::Vector3d ve = ptsB[i] - ptsBcalculated[i];
-        double e = std::sqrt(ve[0] * ve[0] + ve[1] * ve[1] + ve[2] * ve[2]);
-        sum += e;
-    }
-    return sum / ptsB.size();
-}
+#include "coordinateTransform.h"
 
 int main(int argc, char* argv[]) {
     Eigen::Vector3d p1c, p2c, p3c, p4c, p5c, p6c,
@@ -126,40 +23,42 @@ int main(int argc, char* argv[]) {
     p5h = {2.4901, 7.5449, 1.9518};
     p6h = {2.4273, 7.1354, 1.4349};
 
-    Eigen::MatrixXd C1(4, 1), C2(4, 1), C3(4, 1), A(4, 4), transformMat(4, 4);
+    // Eigen::MatrixXd C1(4, 1), C2(4, 1), C3(4, 1), A(4, 4), transformMat(4, 4);
 
-    double phi, psi, theta;
+    // double phi, psi, theta;
     std::vector<Eigen::Vector3d> ptsA, ptsB;
 
     ptsA = {p1c, p2c, p3c, p4c, p5c, p6c};
     ptsB = {p1h, p2h, p3h, p4h, p5h, p6h};
-    std::vector<Eigen::Vector3d> ptsBcalculated;
+    // std::vector<Eigen::Vector3d> ptsBcalculated;
+
+    coordinateTransform t;
+    t.ptsA = ptsA;
+    t.ptsB = ptsB;
 
     std::cout << "----------Transformation Matrix----------" << std::endl;
-
-    calculateTransformMatrix(transformMat, phi, psi, theta, A, C1, C2, C3, ptsA, ptsB);
-    std::cout << transformMat << std::endl;
+    t.calculateTransformMatrix();
+    std::cout << t.transformMat << std::endl;
 
     std::cout << "----------Rotation Angels----------" << std::endl;
-
-    std::cout << "phi : " << phi << " psi : " << psi << " theta : " << theta << std::endl;
+    std::cout << "phi : " << t.phi << " psi : " << t.psi << " theta : " << t.theta << std::endl;
 
     // std::cout << "----------Test----------" << std::endl;
-
     // Eigen::MatrixXd p1(3, 1);
     // p1 << 0.5449, 0.1955, 0.9227;
     // Eigen::Vector3d pp;
-    // transformPointFromSysAtoSysB(transformMat, p1c, pp);
+    // t.transformPointFromSysAtoSysB(p1c, pp);
     // std::cout << pp << std::endl;
 
-    generatedPointsEquivaletPtsB(transformMat, ptsA, ptsB, ptsBcalculated);
+    // t.generatePtsBcalculated();
     // for (int i = 0; i < ptsB.size(); i++) {
     //     std::cout << "----------" << std::endl;
-    //     std::cout << ptsBcalculated[i] << std::endl;
+    //     std::cout << t.ptsBcalculated[i] << std::endl;
     // }
 
     std::cout << "----------Error----------" << std::endl;
-    std::cout << errorCalculation(ptsB, ptsBcalculated) << std::endl;
+    double ee = t.errorCalculation();
+    std::cout << ee << std::endl;
 
     return 0;
 }
